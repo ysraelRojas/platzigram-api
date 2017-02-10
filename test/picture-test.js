e991f4ac5e4145e0e03005149c4423304ec44771
Ysrael Rojas Rojas <yrra_rojas@hotmail.com>
2017-02-10 17:58:05 -0500

    Creacion de rutas para microservicios

---
+++ test/picture-test.js
@@ -0,0 +1,20 @@
+'use strict'
+
+import test from 'ava'
+import micro from 'micro'
+import uuid from 'uuid-base62'
+import listen from 'test-listen'
+import request from 'request-promise'
+import pictures from '../pictures'
+
+test('GET /:id', async t => {
+    let id = uuid.v4()
+    let srv = micro(pictures)
+
+    let url = await listen(srv)
+    let body = await request({uri: `${url}/${id}`, json:true})
+    t.deepEqual(body, {id})
+})
+
+test.todo('POST /')
+test.todo('POST /:id/like')
